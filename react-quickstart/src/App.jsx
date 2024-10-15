0d4d0dfa648a860a88a0afd0de333bd2b5fbf183
Jasmine Manansala <manansala.josephavenir@gmail.com>
2024-10-15 17:01:19 +0800

    I’m done completing the section on rendering lists.

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -40,6 +40,23 @@ function App() {
     content = <LoginForm />;
   }
 
+
+
+  // Rendering lists
+  const products = [
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ];
+
+  const listItems = products.map(product =>
+    <li key={product.id}>
+      {product.title}
+    </li>
+  );
+  
+  
+
   return (
     <>
       <h1>Welcome to my app.</h1>
@@ -54,6 +71,9 @@ function App() {
       {/* Conditional rendering */}
       {content}
       
+      
+      {/* Rendering lists */}
+      <ul>{listItems}</ul>
       
       
     </>

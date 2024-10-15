3c91d0ccb92ecea0e71b3fdf18792b9641a4c8a0
Jasmine Manansala <manansala.josephavenir@gmail.com>
2024-10-15 16:21:25 +0800

    I’m done completing the section on writing markup with JSX.

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,8 +1,22 @@
+// Creating and nesting components
 function MyButton() {
   return (
     <button>I'm a button</button>
   );
 }
+
+
+
+// Writing markup with JSX 
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
 
 
 function App() {
@@ -11,6 +25,7 @@ function App() {
     <>
       <h1>Welcome to my app.</h1>
       <MyButton/>
+      <AboutPage />
     </>
   )
 }
